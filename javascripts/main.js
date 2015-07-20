9005fcf3f79426ccd1b50ac669b4663ad6fb0f1b
Leo McArdle <leomcardle@gmail.com>
2015-07-21 00:01:35 +0100

    some initial graphing work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,71 @@
-console.log('This would be the main JS file.');
+var apiData = {
+  "new_users_by_date": {
+    "2014-12-07": 1,
+    "2014-12-15": 1,
+    "2014-12-16": 1,
+    "2014-12-19": 1,
+    "2014-12-08": 2,
+    "2014-12-20": 1,
+    "2014-12-13": 1,
+    "2015-01-12": 2,
+    "2014-12-14": 1,
+    "2015-01-09": 1,
+    "2014-12-06": 4,
+    "2015-02-07": 1,
+    "2015-07-08": 1
+  }
+}
+
+var oldestDay = moment()
+
+for (var day in apiData.new_users_by_date) {
+  var date = moment(day)
+  if (date < oldestDay)
+    oldestDay = date
+}
+
+var days = moment().diff(oldestDay, 'days')
+var labels = new Array(days)
+var series = new Array(days)
+var lastNumber = 0
+
+for (var index = 0; index < days; index++) {
+  var day = moment(oldestDay).add(index, 'days')
+  var dayString = day.format('YYYY-MM-DD')
+  var number = apiData.new_users_by_date[dayString]
+  if (number) {
+    lastNumber += number
+    series[index] = lastNumber
+  }
+}
+
+for (var index = 0; index < days; index++) {
+  var day = moment(oldestDay).add(index, 'days')
+  var dayString = day.format('MMM ’YY')
+  var dayOfMonth = day.date()
+  if (dayOfMonth === 1)
+    labels[index] = dayString
+}
+
+var data = {
+  labels: labels,
+  series: [
+    series
+  ]
+}
+
+var options = {
+  showPoint: false,
+  //lineSmooth: false,
+  lineSmooth: Chartist.Interpolation.step({
+    postpone: true
+  }),
+  axisX: {
+    onlyInteger: true
+  },
+  axisY: {
+    onlyInteger: true,
+  }
+}
+
+new Chartist.Line('.ct-chart', data, options)

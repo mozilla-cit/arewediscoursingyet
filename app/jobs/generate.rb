a1f00b2c9ee51c706fdc4a2cfa0003fc76b0280f
Leo McArdle <leomcardle@gmail.com>
2015-07-21 18:22:27 +0100

    added logging

--- app/jobs/generate.rb
+++ app/jobs/generate.rb
@@ -1,20 +1,30 @@
+#require_dependency 'benchmark'
+
 module Stats
   class Generate < Jobs::Scheduled
     every 1.minute
 
     def execute(args)
       unless SiteSetting.awdy_enabled
-        puts 'AWDY DISABLED'
+        Rails.logger.info 'AWDY disabled'
         return
       end
 
-      puts 'AWDY GENERATING'
-      stats = { :new_users_by_date => {} }
-      User.all.each do |u|
-        date = u.attributes['created_at'].strftime('%F')
-        stats[:new_users_by_date][date] = stats[:new_users_by_date][date].to_i + 1
-      end
-      ::PluginStore.set('awdy', 'stats', stats)
+      now = Time.new().utc
+
+      Rails.logger.info "AWDY starting stats generation... (#{now})"
+
+      time = Benchmark.realtime {
+        stats = { :new_users_by_date => {} }
+        User.all.each do |u|
+          date = u.attributes['created_at'].strftime('%F')
+          stats[:new_users_by_date][date] = stats[:new_users_by_date][date].to_i + 1
+        end
+        ::PluginStore.set('awdy', 'stats', stats)
+      }
+
+      Rails.logger.info "AWDY finished (took: #{time.round(2)}s)"
+
     end
   end
 end

428d71ac47556051dcb5d027c0b6232d8ccdd823
Leo McArdle <leomcardle@gmail.com>
2015-07-22 20:30:13 +0100

    more d3 stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,34 +1,39 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom
 
 var formatDate = d3.time.format('%Y-%m-%d')
-var parseDate = formatDate.parse;
+var parseDate = formatDate.parse
 
 var x = d3.time.scale()
-        .range([0, width]);
+  .range([0, width])
 
 var y = d3.scale.linear()
-        .range([height, 0]);
+  .range([height, 0])
 
 var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient('bottom');
+  .scale(x)
+  .orient('bottom')
+  .tickSize(-height, 0)
+  .tickPadding(10)
 
 var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('left');
+  .scale(y)
+  .orient('left')
+  .tickSize(-width, 0)
+  .tickPadding(10)
 
 var line = d3.svg.line()
-           .x(function (d) { return x(d.date) })
-           .y(function (d) { return y(d.users) });
+  .x(function (d) { return x(d.date) })
+  .y(function (d) { return y(d.users) })
+  .interpolate('step-after')
 
 var svg = d3.select('body')
-          .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
 d3.json('http://csa-discourse.mofostaging.net/awdy/stats.json', function (error, stats) {
   if (error) throw error
@@ -66,22 +71,22 @@ d3.json('http://csa-discourse.mofostaging.net/awdy/stats.json', function (error,
   y.domain(d3.extent(data, function (d) { return d.users }))
 
   svg.append('g')
-       .attr('class', 'x axis')
-       .attr('transform', 'translate(0,' + height + ')')
-       .call(xAxis);
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
 
   svg.append('g')
-       .attr('class', 'y axis')
-       .call(yAxis)
-     .append('text')
-       .attr('transform', 'rotate(-90)')
-       .attr('y', 6)
-       .attr('dy', '.71em')
-       .style('text-anchor', 'end')
-       .text('Total Users');
+    .attr('class', 'y axis')
+    .call(yAxis)
+  .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .text('Total Users')
 
   svg.append('path')
-       .datum(data)
-       .attr('class', 'line')
-       .attr('d', line);
-});
+    .datum(data)
+    .attr('class', 'line')
+    .attr('d', line)
+})
